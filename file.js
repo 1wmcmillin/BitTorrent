cd34ee278489a1840569d5dbba7752afc31c77a7
1wmcmillin <42749672+1wmcmillin@users.noreply.github.com>
2018-11-12 13:53:56 -0600

    Add files via upload
    second function

---
+++ file.js
@@ -0,0 +1,19 @@
+//type your code inside this function
+function TorrentFile(){
+
+//first function
+function numberOfPackets(fileSize){
+    return fileSize/50; 
+}
+
+
+
+
+//second function
+function buildTorrentPacketArray(numOfPackets){
+
+}
+
+
+
+//main test methods
